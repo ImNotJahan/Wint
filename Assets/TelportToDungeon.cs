90c75b3b01a854b365d7d2fa138eddc6b456100a
ImNotJahan <hiimjahan@gmail.com>
2021-07-07 12:38:19 -0700

    Added a way to go to the dungeon
    Nothings loaded with it currently so you aren't a player yet, but I'll add that later.

---
+++ Assets/TelportToDungeon.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class TelportToDungeon : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
