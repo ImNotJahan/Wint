9bf689f2387b72f072a34794aaa11454c7437d56
ImNotJahan <hiimjahan@gmail.com>
2021-07-13 08:56:30 -0700

    Removed files that were no longer being used
    Also changed minotaur health back to 100. Sidenote: I will need to change the minotaurs values to something better once I start adding more enemies, which I should do soon.

--- Assets/Scripts/Internal/Multiplayer/PlayerManager.cs
+++ Assets/Scripts/Internal/Multiplayer/PlayerManager.cs
@@ -6,7 +6,6 @@ public class PlayerManager : MonoBehaviour
 {
     [SerializeField] GameObject playerCamera = null;
     [SerializeField] CharacterController playerCharacterController = null;
-    [SerializeField] Focus focus = null;
     [SerializeField] PlayerMovementScript playerMovementScript = null;
     [SerializeField] TextMeshPro text = null;
 
@@ -27,7 +26,6 @@ public class PlayerManager : MonoBehaviour
         {
             playerCamera.SetActive(true);
             playerCharacterController.enabled = true;
-            focus.enabled = true;
             playerMovementScript.enabled = true;
 
             mouseLook = playerCamera.GetComponent<MouseLook>();
