75f2e7d23ee8cc269056bca2a85662523ae14797
ImNotJahan <hiimjahan@gmail.com>
2021-08-04 19:35:02 -0700

    Added discord integration and imp model

---
+++ Assets/Scripts/Internal/API/DiscordController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class DiscordJoinEvent : UnityEngine.Events.UnityEvent<string> { }
+
+[Serializable]
+public class DiscordSpectateEvent : UnityEngine.Events.UnityEvent<string> { }
+
+[Serializable]
+public class DiscordJoinRequestEvent : UnityEngine.Events.UnityEvent<DiscordRpc.DiscordUser> { }
+
+public class DiscordController : MonoBehaviour
+{
+    public static int timestamp = 0;
+    public DiscordRpc.RichPresence presence = new DiscordRpc.RichPresence();
+    public string applicationId = "852535107906437140";
+    public string optionalSteamId;
+    public DiscordRpc.DiscordUser joinRequest;
+    public UnityEngine.Events.UnityEvent onConnect;
+    public UnityEngine.Events.UnityEvent onDisconnect;
+    public UnityEngine.Events.UnityEvent hasResponded;
+    public DiscordJoinEvent onJoin;
+    public DiscordJoinEvent onSpectate;
+    public DiscordJoinRequestEvent onJoinRequest;
+
+    DiscordRpc.EventHandlers handlers;
+
+    public void RequestRespondYes()
+    {
+        DiscordRpc.Respond(joinRequest.userId, DiscordRpc.Reply.Yes);
+        hasResponded.Invoke();
+    }
+
+    public void RequestRespondNo()
+    {
+        DiscordRpc.Respond(joinRequest.userId, DiscordRpc.Reply.No);
+        hasResponded.Invoke();
+    }
+
+    public void ReadyCallback(ref DiscordRpc.DiscordUser connectedUser)
+    {
+        onConnect.Invoke();
+        CheckPresence("Slaying Minotaurs");
+    }
+
+    public static void CheckPresence(string state)
+    {
+        int amountOfPlayers = 1;
+        DiscordRpc.RichPresence presence = new DiscordRpc.RichPresence();
+
+        presence.state = state;
+        presence.details = amountOfPlayers == 1 ? "Solo" : "In a party";
+        presence.startTimestamp = timestamp;
+        presence.endTimestamp = 0;
+        presence.smallImageText = "ImNotJahan";
+        presence.largeImageText = "Level 1";
+        presence.largeImageKey = "main";
+        presence.partyId = "ae488379-351d-4a4f-ad32-2b9b01c91657";
+        presence.partySize = amountOfPlayers;
+        presence.partyMax = 5;
+        presence.joinSecret = "MTI4NzM0OjFpMmhuZToxMjMxMjM= ";
+
+        DiscordRpc.UpdatePresence(presence);
+    }
+
+    public void DisconnectedCallback(int errorCode, string message)
+    {
+        onDisconnect.Invoke();
+    }
+
+    public void ErrorCallback(int errorCode, string message)
+    {
+    }
+
+    public void JoinCallback(string secret)
+    {
+        onJoin.Invoke(secret);
+    }
+
+    public void SpectateCallback(string secret)
+    {
+        onSpectate.Invoke(secret);
+    }
+
+    public void RequestCallback(ref DiscordRpc.DiscordUser request)
+    {
+        joinRequest = request;
+        onJoinRequest.Invoke(request);
+    }
+
+    void Start()
+    {
+        timestamp = (int)(DateTime.Now.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+    }
+
+    void Update()
+    {
+        DiscordRpc.RunCallbacks();
+    }
+
+    void OnEnable()
+    {
+        handlers = new DiscordRpc.EventHandlers();
+        handlers.readyCallback += ReadyCallback;
+        handlers.disconnectedCallback += DisconnectedCallback;
+        handlers.errorCallback += ErrorCallback;
+        handlers.joinCallback += JoinCallback;
+        handlers.spectateCallback += SpectateCallback;
+        handlers.requestCallback += RequestCallback;
+        DiscordRpc.Initialize(applicationId, ref handlers, true, optionalSteamId);
+    }
+
+    void OnDisable()
+    {
+        DiscordRpc.Shutdown();
+    }
+
+    void OnDestroy()
+    {
+
+    }
+}
