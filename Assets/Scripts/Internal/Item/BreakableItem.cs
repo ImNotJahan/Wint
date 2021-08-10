83c00f0ec83c20f3c68f445c1fc86e148b4f67ed
ImNotJahan <hiimjahan@gmail.com>
2021-08-10 10:20:51 -0700

    Made the breaking sound finish after the object was destroyed

--- Assets/Scripts/Internal/Item/BreakableItem.cs
+++ Assets/Scripts/Internal/Item/BreakableItem.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class BreakableItem : MonoBehaviour
 {
@@ -20,7 +21,22 @@ public class BreakableItem : MonoBehaviour
 
     private void DestroySelf()
     {
-        Instantiate(drops, transform.position, transform.rotation);
+        Instantiate(drops, transform.position, transform.rotation).SetActive(true);
+
+        GetComponent<Renderer>().enabled = false;
+        GetComponents<Collider>()[0].enabled = false;
+        GetComponents<Collider>()[1].enabled = false;
+
+        StartCoroutine(DestroyAfterAudio());
+    }
+
+    IEnumerator DestroyAfterAudio()
+    {
+        while (GetComponent<AudioSource>().isPlaying)
+        {
+            yield return null;
+        }
+
         Destroy(gameObject);
     }
 }
