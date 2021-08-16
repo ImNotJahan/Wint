cf276f91800bc587f747d24a9c246ae1963e77e3
ImNotJahan <hiimjahan@gmail.com>
2021-08-16 12:52:37 -0700

    Polished up crafting

--- Assets/Scripts/Internal/Player/UI/CraftButton.cs
+++ Assets/Scripts/Internal/Player/UI/CraftButton.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CraftButton : MonoBehaviour
 {
     public List<Item> itemsNeeded = new List<Item>();
     public Item result;
+    public UnityEvent onCraft = new UnityEvent();
 
     public void Craft()
     {
@@ -14,5 +16,6 @@ public class CraftButton : MonoBehaviour
         }
 
         PlayerMovementScript.instance.GetComponent<CharacterCombat>().myStats.inventory.Add(result);
+        onCraft.Invoke();
     }
 }
--- Assets/Scripts/Internal/Player/UI/CraftItem.cs
+++ Assets/Scripts/Internal/Player/UI/CraftItem.cs
@@ -16,9 +16,28 @@ public class CraftItem : MonoBehaviour
     {
         if (count != 1) craftTitle.text = string.Format("{0} {1}x", title.item_id, count.ToString());
         else craftTitle.text = title.item_id;
-        craftIngredients.text = ingredients.ToString();
 
+        string ingredientsList = "";
+
+        foreach(Item ingredient in ingredients)
+        {
+            ingredientsList += ingredient.item_id + "\n";
+        }
+
+        craftIngredients.text = ingredientsList;
+
+        bool hasIngredients = true;
+
+        List<Item> inventoryCopy = PlayerMovementScript.instance.GetComponent<CharacterCombat>().myStats.inventory;
+        foreach (Item item in ingredients)
+        {
+            if (inventoryCopy.Contains(item)) inventoryCopy.Remove(item);
+            else hasIngredients = false;
+        }
+
+        button.GetComponent<Button>().interactable = hasIngredients;
         button.itemsNeeded = ingredients;
         button.result = title;
+        button.onCraft.AddListener(DisplayItem); //to check if the player still has enough ingredients to craft
     }
 }
