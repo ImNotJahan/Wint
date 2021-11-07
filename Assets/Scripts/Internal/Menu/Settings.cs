c65b056d762a07a70cf1de3234c4127eb66cceb8
ImNotJahan <hiimjahan@gmail.com>
2021-11-07 10:23:02 -0800

    Added video settings to pause menu

---
+++ Assets/Scripts/Internal/Menu/Settings.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+    [SerializeField] private Dropdown fullscreenDropdown = null;
+    [SerializeField] private Dropdown qualityPresetDropdown = null;
+
+    public void ChangeFullscreenMode()
+    {
+        switch (fullscreenDropdown.value)
+        {
+            case 0:
+                Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
+                break;
+
+            case 1:
+                Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+                break;
+
+            case 2:
+                Screen.fullScreenMode = FullScreenMode.MaximizedWindow;
+                break;
+
+            case 3:
+                Screen.fullScreenMode = FullScreenMode.Windowed;
+                break;
+
+            default:
+                Debug.LogError("Incorrect Fullscreen Selected");
+                break;
+        }
+    }
+
+    public void ChangeQualityPreset()
+    {
+        QualitySettings.SetQualityLevel(qualityPresetDropdown.value);
+    }
+}
