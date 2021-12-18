89eb5d040b0a21a5ac6f2b0da6f8aedf96fcacfe
ImNotJahan <hiimjahan@gmail.com>
2021-12-18 13:29:30 -0800

    Gave finite enemies an FOV

--- Assets/Scripts/Internal/AI/FiniteEnemy.cs
+++ Assets/Scripts/Internal/AI/FiniteEnemy.cs
@@ -13,37 +13,48 @@ public class FiniteEnemy : MonoBehaviour
 
     private CharacterController controller;
 
+    // For dot product of view
+    public Vector3 direction;
+    public bool inFOV; // FOV = Field of View
+    public bool objectBetweenTargetAndSelf = true;
+
     [SerializeField] private float speed = 1;
     [SerializeField] private Transform head = null;
+
+    private Transform target;
 
     void Start()
     {
         animator = GetComponent<Animator>();
         controller = GetComponent<CharacterController>();
+        target = PlayerMovementScript.instance.gameObject.transform;
     }
 
     void Update()
     {
-        ray.origin = head.position;
-        ray.direction = head.forward;
-        info = animator.GetCurrentAnimatorStateInfo(0);
-        objectInSite = "";
+        direction = (target.position - transform.position).normalized;
+        inFOV = Vector3.Dot(transform.forward.normalized, direction) > .7f;
 
-        Debug.DrawRay(ray.origin, ray.direction * 100, Color.red);
-        if (Physics.Raycast(ray.origin, ray.direction, out hit))
+        Debug.DrawRay(transform.position, direction * 100, Color.red);
+        Debug.DrawRay(transform.position, transform.forward * 100, Color.red);
+
+        if(Physics.Raycast(transform.position, direction * 100, out hit))
         {
-            objectInSite = hit.collider.gameObject.tag;
-            if(objectInSite == "Player")
-            {
-                animator.SetBool("canSeePlayer", true);
-            }
+            if (hit.collider.gameObject.tag == "Player") objectBetweenTargetAndSelf = false;
+            else objectBetweenTargetAndSelf = true;
         }
+
+        if (!objectBetweenTargetAndSelf && inFOV)
+        {
+            animator.SetBool("canSeePlayer", true);
+            head.LookAt(target);
+            transform.LookAt(new Vector3(target.position.x, transform.position.y, target.position.z));
+        }
+        else animator.SetBool("canSeePlayer", false);
 
         if (info.IsName("CHASE"))
         {
-            Transform target = PlayerMovementScript.instance.gameObject.transform;
-            head.LookAt(target);
-            transform.LookAt(new Vector3(target.position.x, transform.position.y, target.position.z));
+            
             if (objectInSite != "Player") animator.SetBool("canSeePlayer", false);
             else controller.SimpleMove(Vector3.forward * Time.deltaTime * speed); //transform.Translate(Vector3.forward * Time.deltaTime * speed);
         }
